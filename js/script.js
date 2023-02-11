723aa844ad63d806f7f36f773b79e18380b11395
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-02-11 21:58:14 +0100

    feat: created account usernames and updated balance user

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
+// DOM ELEMENTS
+const btnSignin = document.querySelector('#btn__signin');
+const mainContainer = document.querySelector('main');
+const labelBalance = document.querySelector('.balance');
+const allTransactionsContainer = document.querySelector(
+  '#customer-transactions-container'
+);
+
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
@@ -36,13 +44,48 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 /***************************************/
-const allTransactionsContainer = document.querySelector(
-  '#customer-transactions-container'
-);
+//Compute and Display balance
+const computeDisplayBalance = function (allTransactions) {
+  const balance = allTransactions.reduce((acc, c) => acc + c, 0);
+  labelBalance.textContent = `${balance} €`;
+};
+computeDisplayBalance(account1.movements);
 
-const mainContainer = document.querySelector('main');
-const btnSignin = document.querySelector('#btn__signin');
+// Create account username
+const createUsernames = function (accs) {
+  accs.forEach(acc => {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
 
+function displayTransactions(allTransactions) {
+  allTransactions.forEach((trans, i) => {
+    const transactionType = trans > 0 ? 'deposit' : 'withdrawal';
+
+    const transactionHTML = `
+    <div class='transaction'>
+      <div>
+        <span class='transaction-type ${transactionType}'>${
+      i + 1
+    } ${transactionType}
+        </span>
+        <span class='date'>12/03/2020</span>
+      </div>
+      <span class='transaction-amount'>${trans} €</span>
+    </div>
+    `;
+
+    allTransactionsContainer.insertAdjacentHTML('afterbegin', transactionHTML);
+  });
+}
+displayTransactions(account1.movements);
+
+/**********
 // USER SIGN IN
 btnSignin.addEventListener('click', function () {
   allTransactionsContainer.innerHTML = '';
@@ -56,26 +99,6 @@ btnSignin.addEventListener('click', function () {
   username.toLowerCase() === 'js' &&
     Number(pin) === 1111 &&
     mainContainer.classList.add('active');
-
-  displayTransactions(account1.movements);
 });
 
-function displayTransactions(allTransactions) {
-  allTransactions.forEach((trans, i) => {
-    const transactionType = trans > 0 ? 'deposit' : 'withdrawal';
-
-    const transactionHTML = `
-      <div class='transaction'>
-        <div>
-          <span class='transaction-type ${transactionType}'>${
-      i + 1
-    } ${transactionType}</span>
-          <span class='date'>12/03/2020</span>
-        </div>
-        <span class='transaction-amount'>${trans}</span>
-      </div>
-    `;
-
-    allTransactionsContainer.insertAdjacentHTML('afterbegin', transactionHTML);
-  });
-}
+***********************/
